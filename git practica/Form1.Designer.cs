cddca8514a38726e6799aecf3d7ec0e5fabb3176
Menta022 <noemail@hotmail.com>
2024-02-13 10:13:00 -0300

    titulo en grilla

--- git practica/Form1.Designer.cs
+++ git practica/Form1.Designer.cs
@@ -29,7 +29,8 @@
         private void InitializeComponent()
         {
             this.dtg_persona = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btn_cargar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dtg_persona)).BeginInit();
             this.SuspendLayout();
             // 
@@ -41,33 +42,45 @@
             this.dtg_persona.Size = new System.Drawing.Size(308, 219);
             this.dtg_persona.TabIndex = 0;
             // 
-            // button1
+            // btn_cargar
             // 
-            this.button1.Location = new System.Drawing.Point(610, 40);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(90, 40);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btn_cargar.Location = new System.Drawing.Point(622, 40);
+            this.btn_cargar.Name = "btn_cargar";
+            this.btn_cargar.Size = new System.Drawing.Size(90, 40);
+            this.btn_cargar.TabIndex = 1;
+            this.btn_cargar.Text = "Cargar";
+            this.btn_cargar.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Personas:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(739, 412);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_cargar);
             this.Controls.Add(this.dtg_persona);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dtg_persona)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dtg_persona;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_cargar;
+        private System.Windows.Forms.Label label1;
     }
 }
 
