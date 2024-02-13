7ae8cc4bb167b49a3847aad84fd9dba8f81aa8ea
matias molina <haseo544444@gmail.com>
2024-02-13 09:18:37 -0300

    inicie git

---
+++ git practica/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace git_practica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtg_persona = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dtg_persona)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtg_persona
+            // 
+            this.dtg_persona.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtg_persona.Location = new System.Drawing.Point(41, 40);
+            this.dtg_persona.Name = "dtg_persona";
+            this.dtg_persona.Size = new System.Drawing.Size(308, 219);
+            this.dtg_persona.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(739, 412);
+            this.Controls.Add(this.dtg_persona);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dtg_persona)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtg_persona;
+    }
+}
+
