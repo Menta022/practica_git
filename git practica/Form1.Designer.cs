0072f63680dd2bbc89b74a343eefd61c518aafd0
Menta022 <haseo544444@gmail.com>
2024-02-14 15:37:14 -0300

    prueba

--- git practica/Form1.Designer.cs
+++ git practica/Form1.Designer.cs
@@ -65,7 +65,7 @@
             // 
             this.button1.Location = new System.Drawing.Point(622, 139);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(90, 23);
+            this.button1.Size = new System.Drawing.Size(90, 40);
             this.button1.TabIndex = 3;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
